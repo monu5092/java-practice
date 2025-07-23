f5a426d0b52979a34a34191a9d4851f09b63763e
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-07-23 11:39:10 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class WhileLoop
+{
+   public static void main(String args[])
+   {
+      int number =0;
+	  while(number<10)
+	  { 
+	     number = number + 1;
+		 System.out.println(number)
+;	  }
+   }
+}

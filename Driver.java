fc2d88e51d2a6f4cb6df283ec12769585884f62b
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-07-24 11:15:54 +0530

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,29 +1,47 @@
 import java.util.Scanner;
 
-class OddEven
-{
-   public void checkOddEven(int number)
-   {
-     if(number%2==0)
-	 {
-	   System.out.print("Number "+number+" is Even.");
-	 }
-	 else{
-	   System.out.print("Number "+number+" is Odd.");
-	 }
-   }
-}
-
 class Driver
 {
   public static void main(String args[])
   {
     Scanner sc = new Scanner(System.in);
 	
-	int n =sc.nextInt();
+	System.out.print("Enter the class name in Digit: ");
+	int standard = sc.nextInt();
 	
-	OddEven oE = new OddEven();
-	
-	oE.checkOddEven(n);
+	Switch obj = new Switch();
+	  obj.allocateRoom(standard);
+  }
+}
+
+class Switch
+{
+  public void allocateRoom(int standard)
+  {
+    switch(standard){
+	 case 1:
+	     System.out.print("Go to 1st Floor Hall");
+	    break;
+	 case 2:
+	    System.out.print("Go to 2nd Floor Hall");
+	    break;
+     case 3:
+	     System.out.print("Go to 3rd Floor Hall");
+	    break;
+     case 4:
+	     System.out.print("Go to 4th Floor Hall");
+	    break;
+     case 5:
+	     System.out.print("Go to 5th Floor Hall");
+	    break;
+     case 6:
+	     System.out.print("Go to 6st Floor Hall");
+	    break;	
+     case 7:
+	     System.out.print("Go to 7th Floor Hall");
+	    break;
+     default:
+        System.out.print("Room Allocation upto 7 class");	 
+	}
   }
 }

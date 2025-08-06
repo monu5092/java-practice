1e66e6325389b50efef9bab957d5a10da7483236
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-08-06 13:10:45 +0530

    Add files via upload

---
+++ PhonePay.java
@@ -0,0 +1,13 @@
+package com.kodewala.Oop.Encapsulation;
+
+public class PhonePay 
+{
+	public static void main(String[] args)
+    {
+	FundTransfer ft = new  FundTransfer();
+	 ft.doCredit(-2000);
+	//ft.balance=-1000;
+	//System.out.println(ft.balance);
+
+    }
+}

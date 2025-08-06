127b0a7241c21ae65bb448d8f200d712c92c7d1f
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-08-06 13:11:02 +0530

    Add files via upload

---
+++ UserCourse.java
@@ -0,0 +1,36 @@
+package com.kodewala.Oop.Encapsulation;
+
+public class UserCourse {
+	
+     private String userName;
+     private String userId;
+     private String courseType;
+     private int amount;
+     
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getCourseType() {
+		return courseType;
+	}
+	public void setCourseType(String courseType) {
+		this.courseType = courseType;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+     
+     
+}

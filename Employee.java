c336bea314bf558737fe2156423ba13235e26132
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-08-06 13:04:56 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,28 @@
+package com.kodewala.Oop.Encapsulation;
+
+public class Employee {
+   
+	private String employeName;
+	private String employeId;
+	private int salary;
+	
+	public String getEmployeName() {
+		return employeName;
+	}
+	public void setEmployeName(String employeName) {
+		this.employeName = employeName;
+	}
+	public String getEmployeId() {
+		return employeId;
+	}
+	public void setEmployeId(String employeId) {
+		this.employeId = employeId;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+}

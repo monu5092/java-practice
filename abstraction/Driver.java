d1fa8876698a405661b079b641a6587de4e5493f
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-08-21 22:14:33 +0530

    Add files via upload

---
+++ abstraction/Driver.java
@@ -0,0 +1,23 @@
+package com.kodewala.abstraction;
+
+
+abstract class College
+{
+    public abstract void department();	
+}
+
+class Agriculture extends College
+{
+      public void department() {
+    	  System.out.println("This is Agriculture Department.");
+      }
+}
+public class Driver {
+	
+	public static void main(String args[])
+	{
+		Agriculture ag = new Agriculture();
+		ag.department();
+	}
+
+}

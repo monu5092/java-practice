00351f763cfcce4bb061ccd8e9d3b9528471dc58
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-09-10 22:37:02 +0530

    Add files via upload

---
+++ linkedlist/LinkedListExample.java
@@ -0,0 +1,20 @@
+package com.kodewala.linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListExample 
+{
+  public static void main(String args[])
+  {
+	  LinkedList<String> linkedlist = new LinkedList<>();
+	  
+	  linkedlist.add("Ahm");
+	  linkedlist.add("Baanglore");
+	  linkedlist.add("Che");
+	  linkedlist.add("Del");
+	  linkedlist.add("Gurugram");
+	  
+	  System.out.print(linkedlist);
+	  
+  }
+}

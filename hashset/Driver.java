5583f45d79e373b5c6c916981d1b8c87effecd46
monu5092 <128527715+monu5092@users.noreply.github.com>
2025-09-15 19:33:52 +0530

    Add files via upload

---
+++ hashset/Driver.java
@@ -0,0 +1,29 @@
+package com.kodewala.hashset;
+
+import java.util.HashSet;
+
+public class Driver 
+{
+  public static void main(String args[])
+  {
+	  HashSet<String>  set = new HashSet<>();
+	  
+	  set.add("Book");
+	  set.add("NoteBook");
+	  set.add("pen");
+	  set.add("Bag");
+	  set.add("shoe");
+	  set.add("slipper");
+	  set.add("Box");
+	  set.add("Plate");
+	  set.add("Mattress");
+	  set.add("Pillo");
+	  set.add("Bucket");
+	  set.add("Mirror");
+	  set.add("HandBag");
+	  
+	  System.out.println("Size of the set: "+set.size());
+	  
+	  
+  }
+}
